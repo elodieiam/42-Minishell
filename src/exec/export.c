01274dfc087aaabe5a6371016c43924003c76f15
el0die21 <elodie15@gmail.com>
2023-11-13 15:57:51 +0100

    fix updating env value when exporting a var that already exists

--- src/exec/export.c
+++ src/exec/export.c
@@ -6,39 +6,11 @@
 /*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:08:31 by elrichar          #+#    #+#             */
-/*   Updated: 2023/11/11 18:48:36 by elrichar         ###   ########.fr       */
+/*   Updated: 2023/11/13 15:56:55 by elrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
-int	update_env_value(t_data *data, t_node *node)
-{
-	char	**new_env;
-	int		nb_arg;
-	int		i;
-
-	new_env = NULL;
-	nb_arg = 0;
-	i = 0;
-	while (data->env->envtab[nb_arg])
-		nb_arg++;
-	new_env = malloc(sizeof(char *) * (nb_arg + 1));
-	if (!new_env)
-		return (0);
-	while (i < nb_arg - 1)
-	{
-		new_env[i] = ft_strdup(data->env->envtab[i]);
-		i++;
-	}
-	new_env[i] = ft_strdup(node->command->arguments[1]);
-	new_env[nb_arg] = NULL;
-	if (data->env->malloced == 1)
-		free_dchartab(data->env->envtab);
-	data->env->envtab = new_env;
-	data->env->malloced = 1;
-	return (1);
-}
 
 int	update_env(t_data *data, t_node *node)
 {
@@ -69,26 +41,38 @@ int	update_env(t_data *data, t_node *node)
 
 int	var_already_set(t_data *data, t_node *node)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*tmp;
 
 	i = 0;
 	j = 0;
+	tmp = NULL;
 	while (node->command->arguments[1][i] != '=')
 		i++;
 	while (data->env->envtab[j])
 	{
-		if (!ft_strncmp(node->command->arguments[1], data->env->envtab[j], i))
+		if (!ft_strncmp(node->command->arguments[1], data->env->envtab[j], i + 1))
+		{
+			tmp = malloc(sizeof(char) * ft_strlen(node->command->arguments[1]) + 1);
+			if (!tmp)
+				return (-1);
+			ft_strlcpy(tmp, node->command->arguments[1], ft_strlen(node->command->arguments[1]) + 1);
+			if (data->env->malloced)
+				free(data->env->envtab[j]);
+			data->env->envtab[j] = tmp;
 			return (1);
+		}
 		j++;
 	}
 	return (0);
 }
 
-//handle error if malloc fails
+
 int	exec_export(t_data *data, t_node *node)
 {
 	int	i;
+	int	set;
 
 	i = 1;
 	// if (!node->command->arguments[i])
@@ -97,14 +81,15 @@ int	exec_export(t_data *data, t_node *node)
 	{
 		if (is_valid_arg(node->command->arguments[i]))
 		{
+			set = var_already_set(data, node);
 			printf("is valid arg\n");
-			if (var_already_set(data, node))
-			{
-				if (!update_env_value(data, node))
-					return (1);
-			}
-			else if (!update_env(data, node))
+			if (set == -1)
 				return (1);
+			else if (set == 0)
+			{
+				if (!update_env(data, node))
+					return (1);		
+			}
 		}
 		else
 			return (1);
