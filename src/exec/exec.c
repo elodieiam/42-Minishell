512221b87ccdfbdaed107078971589674ace6e23
Moyaxzan <tao.spa2003@gmail.com>
2023-11-20 18:52:33 +0100

    builtins exit code kept + expand rds

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 16:27:51 by elrichar          #+#    #+#             */
-/*   Updated: 2023/11/08 16:08:02 by elrichar         ###   ########.fr       */
+/*   Updated: 2023/11/20 18:48:22 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ int	srch_builtin(t_data *data, t_node *node)
 	}
 	if (!ft_strncmp(node->command->arguments[0], "echo", 5))
 	{
-		exec_echo(node);
+		data->err_code = exec_echo(node);
 		return (1);
 	}
 	if (!ft_strncmp(node->command->arguments[0], "export", 7))
 	{
-		exec_export(data, node);
+		data->err_code = exec_export(data, node);
 		return (1);
 	}
 	return (0);
@@ -36,12 +36,9 @@ int	srch_builtin(t_data *data, t_node *node)
 
 int	exec_command(t_data *data, t_node *node)
 {
-	int	err_code;
-
-	err_code = srch_builtin(data, node);
-	if (!err_code)
-		err_code = execute(data, node);
-	return (err_code);
+	if (!srch_builtin(data, node))
+		data->err_code = execute(data, node);
+	return (data->err_code);
 }
 
 int	exec(t_data *data)
