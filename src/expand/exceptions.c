34ad7968e721cda39c79d258fcc9d9258ccfa0fd
Moyaxzan <tao.spa2003@gmail.com>
2023-11-27 18:12:39 +0100

    started expand of exceptions

--- src/expand/exceptions.c
+++ src/expand/exceptions.c
@@ -1,28 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expand.h                                           :+:      :+:    :+:   */
+/*   exceptions.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsaint-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 16:50:45 by tsaint-p          #+#    #+#             */
-/*   Updated: 2023/11/27 16:43:39 by tsaint-p         ###   ########.fr       */
+/*   Created: 2023/11/27 16:42:46 by tsaint-p          #+#    #+#             */
+/*   Updated: 2023/11/27 16:43:01 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXPAND_H
-# define EXPAND_H
-# include "structs.h"
+#include <minishell.h>
 
-/*--------------expand.c--------------*/
-int		expand(t_node *node, char **env);
 
-/*------------exceptions.c------------*/
-
-/*-----------expand_utils.c-----------*/
-int		is_varsep(char c);
-int		nonvarlen(char *str, int quote);
-char	*get_nonvarstr(char *str, int *i, int *quote);
-char	*get_varstr(char *str, int *i, char **env);
-
-#endif
