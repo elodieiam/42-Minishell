4fee8f3bef3f78992e54ebb902096450378c23df
el0die21 <elodie15@gmail.com>
2023-12-04 14:11:13 +0100

    fix export

--- src/exec/exec_env.c
+++ src/exec/exec_env.c
@@ -6,11 +6,23 @@
 /*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:45:59 by elrichar          #+#    #+#             */
-/*   Updated: 2023/11/20 17:07:17 by elrichar         ###   ########.fr       */
+/*   Updated: 2023/12/04 12:57:14 by elrichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+int	is_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] && str[i] != '=')
+		i++;
+	if (str[i] == '=')
+		return (1);
+	return (0);
+}
 
 int	exec_env(t_data *data)
 {
@@ -19,7 +31,8 @@ int	exec_env(t_data *data)
 	i = 0;
 	while (data->env->envtab[i])
 	{
-		printf("%s\n", data->env->envtab[i]);
+		if (is_printable(data->env->envtab[i]))
+			printf("%s\n", data->env->envtab[i]);
 		i++;
 	}
 	return (1);
