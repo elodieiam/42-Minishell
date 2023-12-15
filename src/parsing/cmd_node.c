516300dc28a4e646600802f935981a7d87aad4d6
Moyaxzan <tao.spa2003@gmail.com>
2023-12-15 20:35:54 +0100

    fixed parsing removing indicator

--- src/parsing/cmd_node.c
+++ src/parsing/cmd_node.c
@@ -6,7 +6,7 @@
 /*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 18:23:43 by taospa            #+#    #+#             */
-/*   Updated: 2023/12/15 20:14:39 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/12/15 20:29:24 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ t_node	*fill_cmd_node(t_token **token, t_node *res, int *arg_cpt)
 	return (res);
 }
 
-t_node	*init_cmd_node(t_token **tokens, int malloc_size, int indicator)
+t_node	*init_cmd_node(t_token **tokens, int malloc_size)
 {
 	t_node	*res;
 	int		arg_cpt;
@@ -63,17 +63,11 @@ t_node	*init_cmd_node(t_token **tokens, int malloc_size, int indicator)
 	res->arguments = malloc(malloc_size * sizeof(char *));
 	if (!res->arguments)
 		return (NULL);
-	if (indicator == 1)
-	{
-		free(res->arguments);
-		res->arguments = NULL;
-	}
 	while ((*tokens) && (*tokens)->type != T_PIPE && (*tokens)->type != T_CLPAR
 		&& (*tokens)->type != T_OR && (*tokens)->type != T_AND)
 		if (!fill_cmd_node(tokens, res, &arg_cpt))
 			return (NULL);
-	if (indicator == 0)
-		res->arguments[arg_cpt] = NULL;
+	res->arguments[arg_cpt] = NULL;
 	return (res);
 }
 
@@ -81,15 +75,11 @@ t_node	*handlecommand(t_data *data)
 {
 	t_token	*curr;
 	int		malloc_size;
-	int		indicator;
 
 	curr = data->tokens;
 	malloc_size = 0;
-	indicator = 0;
 	if (curr && curr->type == T_OPPAR)
 		return (handlepar(data));
-	if (curr && (curr->type >= 7 && curr->type <= 10))
-		indicator = 1;
 	while (curr && (curr->type < 4 || curr->type > 6) && curr->type != T_CLPAR)
 	{
 		if (curr->type == T_WORD)
@@ -97,11 +87,11 @@ t_node	*handlecommand(t_data *data)
 		else if (curr->type > 6 && curr->type < 11)
 		{
 			// if (!curr->next || curr->next->type != T_WORD)
-			// 	return (exit_line(data, \
+			// 	return (exit_line(data,
 			// 		errnl(2, "syntax error, no file after redirection")), NULL);
 			curr = curr->next;
 		}
 		curr = curr->next;
 	}
-	return (init_cmd_node(&(data->tokens), ++malloc_size, indicator));
+	return (init_cmd_node(&(data->tokens), ++malloc_size));
 }
