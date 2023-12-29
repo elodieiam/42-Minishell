badf8196d9f9ae83335feefcfcb33d209249fb41
Moyaxzan <tao.spa2003@gmail.com>
2023-12-29 16:13:00 +0100

    pwd works when several arguments

--- src/exec/builtins/pwd.c
+++ src/exec/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: taospa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:31:18 by taospa            #+#    #+#             */
-/*   Updated: 2023/12/15 19:37:38 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/12/29 16:05:16 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ int	exec_pwd(t_data *data, t_node *node)
 {
 	char	*pwd;
 
-	if (node->arguments[1])
-		return (exit_line(data, errnl(1, "pwd: too many arguments")));
+	(void)node;
 	pwd = malloc(sizeof(char) * PATH_MAX);
 	if (!pwd)
 		return (exit_line(data, errnl(UNKNOWN_ERR, "malloc failed")));
